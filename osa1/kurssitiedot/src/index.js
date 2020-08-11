e06eac6ff298f7156629f7348f5434b5c943c6ef
Panu Valtanen <p4nu@users.noreply.github.com>
2020-08-11 09:42:34 +0300

    1.3: kurssitiedot step3 tehty

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -12,7 +12,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercise}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
@@ -20,17 +20,11 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <section>
-      <Part part={props.part1}
-            exercise={props.exercises1}
-      />
+      <Part part={props.part1} />
 
-      <Part part={props.part2}
-            exercise={props.exercises2}
-      />
+      <Part part={props.part2} />
 
-      <Part part={props.part3}
-            exercise={props.exercises3}
-      />
+      <Part part={props.part3} />
     </section>
   )
 }
@@ -47,12 +41,19 @@ const Footer = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  };
 
   return (
     <>
@@ -61,14 +62,11 @@ const App = () => {
       <Content part1={part1}
                part2={part2}
                part3={part3}
-               exercises1={exercises1}
-               exercises2={exercises2}
-               exercises3={exercises3}
       />
 
-      <Footer exercises1={exercises1}
-              exercises2={exercises2}
-              exercises3={exercises3}
+      <Footer exercises1={part1.exercises}
+              exercises2={part2.exercises}
+              exercises3={part3.exercises}
       />
     </>
   )
