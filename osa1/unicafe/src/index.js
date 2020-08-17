3e7b600b20e8a57319756c5961c0e0c5e5b91dbd
Panu Valtanen <p4nu@users.noreply.github.com>
2020-08-17 12:49:30 +0300

    1.6: unicafe step1 tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,15 +1,76 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>{text}</button>
+)
+
+const Feedback = (props) => {
+  const {good, setGood, neutral, setNeutral, bad, setBad} = props;
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  }
+
+  return (
+    <>
+      <h1>Give feedback</h1>
+
+      <Button handleClick={handleGoodClick}
+              text='Good'
+      />
+
+      <Button handleClick={handleNeutralClick}
+              text='Neutral'
+      />
+
+      <Button handleClick={handleBadClick}
+              text='Bad'
+      />
+    </>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <>
+      <h1>Statistics</h1>
+
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <Feedback good={good}
+                setGood={setGood}
+                neutral={neutral}
+                setNeutral={setNeutral}
+                bad={bad}
+                setBad={setBad}
+      />
+
+      <Statistics good={good}
+                  neutral={neutral}
+                  bad={bad}
+      />
+    </>
   )
 }
 
