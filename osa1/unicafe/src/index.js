06c97e9bf6de901a8b19f0ca8fc0568c33cb4f58
Panu Valtanen <p4nu@users.noreply.github.com>
2020-08-17 13:21:39 +0300

    1.8-1.9: unicafe step4 tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -44,6 +44,16 @@ const Statistics = ({good, neutral, bad}) => {
   const average = (good - bad) / total;
   const positivePercent = good / total * 100;
 
+  if (total === 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
+
+        <p>No feedback given.</p>
+      </>
+    )
+  }
+
   return (
     <>
       <h1>Statistics</h1>
