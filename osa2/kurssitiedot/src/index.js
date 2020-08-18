bb34d5a10a4196efda293a3f15faf882877a1635
Panu Valtanen <p4nu@users.noreply.github.com>
2020-08-18 12:04:16 +0300

    2.1: kurssitiedot initial commit

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let total = 0;
+
+const Header = (props) => {
+  return (
+    <header>
+      <h1>{props.course}</h1>
+    </header>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <section>
+      <Part part={props.parts[0]} />
+
+      <Part part={props.parts[1]} />
+
+      <Part part={props.parts[2]} />
+    </section>
+  )
+}
+
+const Footer = (props) => {
+  props.parts.forEach(prop => {
+    total += prop.exercises;
+  })
+
+  return (
+    <footer>
+      <p>
+        Number of exercises {total}
+      </p>
+    </footer>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Header course={course.name} />
+
+      <Content parts={course.parts} />
+
+      <Footer parts={course.parts} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
