70b43206072ead12de1eb4e9df968230c7d58f96
Panu Valtanen <p4nu@users.noreply.github.com>
2020-08-18 12:38:12 +0300

    2.1: kurssitiedot step6 tehty

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -3,74 +3,91 @@ import ReactDOM from 'react-dom';
 
 let total = 0;
 
-const Header = (props) => {
+const Header = ({course}) => {
   return (
     <header>
-      <h1>{props.course}</h1>
+      <h1>{course}</h1>
     </header>
   )
 }
 
-const Part = (props) => {
+const Part = ({part}) => {
   return (
     <p>
-      {props.part.name} {props.part.exercises}
+      {part.name} {part.exercises}
     </p>
   )
 }
 
-const Content = (props) => {
+const Content = ({parts}) => {
   return (
     <section>
-      <Part part={props.parts[0]} />
+      <Part part={parts[0]} />
 
-      <Part part={props.parts[1]} />
+      <Part part={parts[1]} />
 
-      <Part part={props.parts[2]} />
+      <Part part={parts[2]} />
+
+      <Part part={parts[3]} />
     </section>
   )
 }
 
-const Footer = (props) => {
-  props.parts.forEach(prop => {
-    total += prop.exercises;
+const NumberOfExercises = ({parts}) => {
+  parts.forEach(part => {
+    total += part.exercises;
   })
 
   return (
-    <footer>
-      <p>
-        Number of exercises {total}
-      </p>
-    </footer>
+    <p>
+      Number of exercises {total}
+    </p>
+  )
+}
+
+const Course = ({course}) => {
+  return(
+    <>
+      <Header course={course.name} />
+
+      <Content parts={course.parts} />
+
+      <NumberOfExercises parts={course.parts} />
+    </>
   )
 }
 
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
+        id: 1,
         name: 'Fundamentals of React',
         exercises: 10,
       },
       {
+        id: 2,
         name: 'Using props to pass data',
         exercises: 7,
       },
       {
+        id: 3,
         name: 'State of a component',
         exercises: 14,
+      },
+      {
+        id: 4,
+        name: 'TypeScript',
+        exercises: 16,
       },
     ],
   };
 
   return (
     <>
-      <Header course={course.name} />
-
-      <Content parts={course.parts} />
-
-      <Footer parts={course.parts} />
+      <Course course={course} />
     </>
   )
 }
